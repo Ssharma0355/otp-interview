a37bf5f6d4ab22b8172fe648cc8587a9ab6daffd
sachin_sde <ssachin.s@myclnq.co.in>
2025-05-31 20:17:25 +0530

    search my name

--- src/components/GetAllUsers.js
+++ src/components/GetAllUsers.js
@@ -6,14 +6,23 @@ import { increment } from './redux/counterSlice';
 
 function GetAllUsers() {
     const [getdata, setGetData] = useState([]);
+    const [user,setUser] =useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
   const dispatch = useDispatch();
 
     const getUsers = ()=>{
         axios
-          .get("https://dummyjson.com/users")
+          .get("https://dummyjson.com/users" )
           .then((response) => setGetData(response.data.users)); // if the data is like {users:{},{}}
     }
-    console.log(getdata);
+   const findUser = (value) => {
+     axios
+       .get("https://dummyjson.com/users/search?q=" + value)
+       .then((response) => setUser(response.data.users))
+       .catch((err) => console.error(err));
+   };
+
+    console.log(getdata,"getdata");
     useEffect(()=>{
         getUsers();
     },[]);
@@ -21,17 +30,42 @@ function GetAllUsers() {
         dispatch(increment());
         
     }
+  
+    console.log(user, "users");
+   const searchUser = (value) => {
+     setSearchTerm(value);
+     if (value.trim()) {
+       findUser(value);
+     } else {
+       setUser([]);
+     }
+   };
+
 
   return (
-    <div className='Profile-Container'>
+    <div className="Profile-Container">
+      <div>
+        <h1>Search user</h1>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => searchUser(e.target.value)}
+        />
+
+        {user.map((user, index) => (
+          <p key={index}>{user.firstName}</p>
+        ))}
+      </div>
       {getdata.map((d, index) => (
-        <div className='Profile-card' key={index}>
+        <div className="Profile-card" key={index}>
           <img src={d.image} alt={d.firstName} />
           <h1>
             {d.firstName} {d.lastName}
           </h1>
           <h3>{d.email}</h3>
-          <button onClick={addDev} className='Btn-hire'>Add to hire</button>
+          <button onClick={addDev} className="Btn-hire">
+            Add to hire
+          </button>
         </div>
       ))}
     </div>
